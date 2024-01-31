882b899abce1e755eaead55b5901b8086d3888ce
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-01-31 11:34:00 +0000

    opens and reads csv, stores questions, answers and user answers into slices

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	questions := []string{}
+	answers := []string{}
+	userAnswers := []string{}
+
+	for _, questionAnswer := range records {
+		questions = append(questions, questionAnswer[0])
+		answers = append(answers, questionAnswer[1])
+	}
+
+	for _, question := range questions {
+		var input string
+		fmt.Println(question)
+		fmt.Scan(&input)
+		userAnswers = append(userAnswers, input)
+	}
+
+	fmt.Println(answers, userAnswers)
+}
